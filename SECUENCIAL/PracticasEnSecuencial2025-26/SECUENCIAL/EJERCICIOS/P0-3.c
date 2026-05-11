e8acdce90206d3524b38bb0cc75ba9368d80972d
lorenaalmoguera <lorenaalmoguera@gmail.com>
2026-05-11 11:09:36 +0200

    Borrado de PDFs XD y reemplazo por fotos. Práctica 3 secuencial completada

--- SECUENCIAL/PracticasEnSecuencial2025-26/SECUENCIAL/EJERCICIOS/P0-3.c
+++ SECUENCIAL/PracticasEnSecuencial2025-26/SECUENCIAL/EJERCICIOS/P0-3.c
@@ -47,17 +47,81 @@ int main(int argc, char* argv[]) {
 
     for(int i = 0 ; i < dim ; i++) fread(m[i], sizeof(double), dim, f);
 
-    for(int i = 0 ; i < dim ; i++){
+    /*for(int i = 0 ; i < dim ; i++){
         for(int j = 0 ; j < dim ; j++){
             printf("M[%d , %d] = %.8lf", i, j, m[i][j]);
         }
         printf("\n");
-    }
+    }*/
 
     fclose(f);
+
+    double **m_aux = (double**)malloc(filas * sizeof(double*));
+    for(int i = 0 ; i < filas ; i++){
+        m_aux[i] = (double*)malloc(columnas * sizeof(double));
+    }
+
+    int fila_aux = 0;
+
+    for(int i = 0 ; i < dim ; i++){
+
+        if(i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 7 || i == 9 ||
+        i == dim-1 || i == dim-2 || i == dim-3) continue;
+
+        int col_aux = 0;
+
+        for(int j = 0 ; j < dim ; j++){
+
+            if(j == 0 || j == 3 || j == 4 || j == 6 ||
+            j == dim-1 || j == dim-2 || j == dim-3) continue;
+
+            memcpy(&m_aux[fila_aux][col_aux], &m[i][j], sizeof(double)); // void*, void*
+
+            col_aux++;
+        }
+
+        fila_aux++;
+    }
+
+    FILE * f2 = fopen("matrizreducida1.bin", "wb");
+
+    for(int i = 0 ; i < filas ; i++){
+        fwrite(m_aux[i], sizeof(double), columnas, f2);
+    }
+
+    fclose(f2);
+
+    double *vector = (double*)malloc(filas*columnas*sizeof(double));
+
+    int pos = 0;
+
+    for(int i = 0 ; i < dim ; i++){
+        if(i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 7 || i == 9 ||
+        i == dim-1 || i == dim-2 || i == dim-3) continue;
+
+        for(int j = 0 ; j < dim ; j++){
+
+            if(j == 0 || j == 3 || j == 4 || j == 6 ||
+            j == dim-1 || j == dim-2 || j == dim-3) continue;
+
+            memcpy(&vector[pos], &m[i][j], sizeof(double)); // void*, void*
+
+            pos++;
+        }
+    }
+
+    FILE * f3 = fopen("vectorreducido1.bin","wb");
+    fwrite(vector, sizeof(double), filas * columnas, f3);
+
+
+    fclose(f3);
 
     for(int i = 0; i < dim; i++) free(m[i]);
     free(m);
 
+    for(int i = 0 ; i < filas ; i++) free(m_aux[i]);
+    free(m_aux);
+
+    free(vector);
     return 0;
 }
